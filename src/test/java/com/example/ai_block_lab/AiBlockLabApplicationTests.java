f10d0302dd50dce5d2776b4e109c87165b485728
eunalove <88129325+eunalove@users.noreply.github.com>
2023-04-19 21:28:44 +0900

    Build SpringBoot with Java 17 and Gradle

---
+++ src/test/java/com/example/ai_block_lab/AiBlockLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_block_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBlockLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
