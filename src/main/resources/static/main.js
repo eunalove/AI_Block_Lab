c530868453a6703d2c599d0ac9ca2666154fb369
eunalove <88129325+eunalove@users.noreply.github.com>
2023-04-24 10:20:32 +0900

    #2 회원가입&로그인 화면 1차 구현 완료

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -8,3 +8,24 @@ document.getElementById("start-button").addEventListener("click", function() {
         // 닉네임이 입력된 후 메인 화면에서 필요한 작업을 수행
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById('join-box');
+    var btn = document.getElementById('join-button');
+    var span = document.getElementById('closeModal');
+
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
+
