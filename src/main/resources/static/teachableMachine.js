3208cbefa6a20769211d4b14cffdcd6ea019848e
eunalove <88129325+eunalove@users.noreply.github.com>
2023-05-25 17:05:21 +0900

    #30 시작하기 블록 바로 아래의 블록만 실행되고 그 이후 블록은 에러가 발생함
    Uncaught SyntaxError: Invalid code generated: [object Promise] 에러 발생

--- src/main/resources/static/teachableMachine.js
+++ src/main/resources/static/teachableMachine.js
@@ -1,78 +1,69 @@
+let model, webcam, labelContainer, maxPredictions;
 
-function myFunction() {
+let isIos = false;
+// fix when running demo in ios, video will be frozen;
+if (window.navigator.userAgent.indexOf('iPhone') > -1 || window.navigator.userAgent.indexOf('iPad') > -1) {
+    isIos = true;
+}
 
-    let model, webcam, labelContainer, maxPredictions;
-    let isIos = false;
+init();
 
-    init("https://teachablemachine.withgoogle.com/models/okWkNoAb-k/");
+// Load the image model and setup the webcam
+async function init() {
+    const modelURL = URL + 'model.json';
+    const metadataURL = URL + 'metadata.json';
 
+    // load the model and metadata
+    // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
+    // or files from your local hard drive
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-// Check if the platform is iOS
-    if (window.navigator.userAgent.indexOf('iPhone') > -1 || window.navigator.userAgent.indexOf('iPad') > -1) {
-        isIos = true;
+    // Convenience function to setup a webcam
+    const flip = true; // whether to flip the webcam
+    const width = 500;
+    const height = 500;
+    webcam = new tmImage.Webcam(width, height, flip);
+    await webcam.setup(); // request access to the webcam
+
+    if (isIos) {
+        document.getElementById('webcam-container').appendChild(webcam.webcam); // webcam object needs to be added in any case to make this work on iOS
+        // grab video-object in any way you want and set the attributes
+        const webCamVideo = document.getElementsByTagName('video')[0];
+        webCamVideo.setAttribute("playsinline", true); // written with "setAttribute" bc. iOS buggs otherwise
+        webCamVideo.muted = "true";
+        webCamVideo.style.width = width + 'px';
+        webCamVideo.style.height = height + 'px';
+    } else {
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
     }
-
-// Function to start the video
-    async function startVideo() {
-        const flip = true;
-        const width = 500;
-        const height = 500;
-        webcam = new tmImage.Webcam(width, height, flip);
-        await webcam.setup();
-
-        if (isIos) {
-            document.getElementById('webcam-container').appendChild(webcam.webcam);
-            const webCamVideo = document.getElementsByTagName('video')[0];
-            webCamVideo.setAttribute("playsinline", true);
-            webCamVideo.muted = "true";
-            webCamVideo.style.width = width + 'px';
-            webCamVideo.style.height = height + 'px';
-        } else {
-            document.getElementById("webcam-container").appendChild(webcam.canvas);
-        }
-        webcam.play();
+    // append elements to the DOM
+    labelContainer = document.getElementById('label-container');
+    for (let i = 0; i < maxPredictions; i++) { // and class labels
+        labelContainer.appendChild(document.createElement('div'));
     }
+    webcam.play();
+    window.requestAnimationFrame(loop);
+}
 
-// Function to load the model
-    async function loadImageModel(URL) {
-        const modelURL = URL + 'model.json';
-        const metadataURL = URL + 'metadata.json';
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the image model
+async function predict() {
+    // predict can take in an image, video or canvas html element
+    let prediction;
+    if (isIos) {
+        prediction = await model.predict(webcam.webcam);
+    } else {
+        prediction = await model.predict(webcam.canvas);
     }
-
-// Function to perform prediction and update the label
-    async function predictAndUpdateLabel() {
-        let prediction;
-        if (isIos) {
-            prediction = await model.predict(webcam.webcam);
-        } else {
-            prediction = await model.predict(webcam.canvas);
-        }
-        for (let i = 0; i < maxPredictions; i++) {
-            const classPrediction = prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
-            labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
+    for (let i = 0; i < maxPredictions; i++) {
+        const classPrediction =
+            prediction[i].className + ': ' + prediction[i].probability.toFixed(2);
+        labelContainer.childNodes[i].innerHTML = classPrediction;
     }
-
-// Function to start the loop
-    function startLoop() {
-        webcam.update();
-        predictAndUpdateLabel();
-        window.requestAnimationFrame(startLoop);
-    }
-
-// Initialize the application
-    async function init(URL) {
-        labelContainer = document.getElementById('label-container');
-        await loadImageModel(URL);
-        await startVideo();
-        for (let i = 0; i < maxPredictions; i++) {
-            labelContainer.appendChild(document.createElement('div'));
-        }
-        window.requestAnimationFrame(startLoop);
-    }
-
-
-
 }
