e278ab04b738416c9c69297199cf9a9bbbfdada1
eunalove <88129325+eunalove@users.noreply.github.com>
2023-05-28 22:52:43 +0900

    [Fix #33] print블록 실행완료
    공식문서에 나온 javascriptGenerator대신 Blockly.JavaScript를 사용하여 에러를 해결함_JS파일

---
+++ src/main/resources/static/blockEvent.js
@@ -0,0 +1,158 @@
+
+Blockly.JavaScript['start'] = function(block) {
+    console.log("start");
+    return 'start();\n';
+};
+
+Blockly.JavaScript['learning_image_ai'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'learningImageAI();\n';
+    return code;
+};
+
+Blockly.JavaScript['turn_on_the_video'] = function(block) {
+    console.log("turnOnTheVideo");
+
+    return 'turnOnTheVideo();\n';
+};
+
+Blockly.JavaScript['url_image_receive'] = function(block) {
+    console.log("url_image_receive");
+    return 'urlImageReceive();\n';
+};
+
+
+Blockly.JavaScript['classify_by_trained_model_image'] = function(block) {
+    console.log("classifyByTrainedModelImage");
+
+    return 'classifyByTrainedModelImage();\n';
+
+};
+
+Blockly.JavaScript['when_detecting_image'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'when_detecting_image();\n';
+    return code;
+};
+
+Blockly.JavaScript['of_confidence_image'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'of_confidence_image();\n';
+    return code;
+};
+
+Blockly.JavaScript['classification_result_image'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'classification_result_image();\n';
+    return code;
+};
+
+Blockly.JavaScript['class_learned_image'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'class_learned_image();\n';
+    return code;
+};
+
+
+Blockly.JavaScript['learning_voice_ai'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'learning_voice_ai();\n';
+    return code;
+};
+Blockly.JavaScript['learning_text_ai'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'learning_text_ai();\n';
+    return code;
+};
+
+Blockly.JavaScript['classify_by_trained_model_voice'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'classify_by_trained_model_voice();\n';
+    return code;
+};Blockly.JavaScript['classify_by_trained_model_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'classify_by_trained_model_text();\n';
+    return code;
+};Blockly.JavaScript['turn_on_the_voice'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'turn_on_the_voice();\n';
+    return code;
+};
+Blockly.JavaScript['turn_on_the_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'turn_on_the_text();\n';
+    return code;
+};
+
+
+Blockly.JavaScript['when_detecting_voice'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'when_detecting_voice();\n';
+    return code;
+};
+
+Blockly.JavaScript['when_detecting_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'when_detecting_text();\n';
+    return code;
+};
+
+
+Blockly.JavaScript['of_confidence_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'of_confidence_text();\n';
+    return code;
+};
+
+
+Blockly.JavaScript['classification_result_voice'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'classification_result_voice();\n';
+    return code;
+};
+
+Blockly.JavaScript['classification_result_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'classification_result_text();\n';
+    return code;
+};
+
+
+Blockly.JavaScript['class_learned_voice'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'class_learned_voice();\n';
+    return code;
+};
+
+Blockly.JavaScript['class_learned_text'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'class_learned_text();\n';
+    return code;
+};
+
+Blockly.JavaScript['combine_letters'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'combine_letters();\n';
+    return code;
+};
+//뭐임..?
+Blockly.JavaScript['print'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_NONE) || '""';
+    // var text = block.getFieldValue('TEXT');
+    console.log(text);
+    return 'print();\n';
+};
+
+Blockly.JavaScript['show_results_together'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'show_results_together();\n';
+    return code;
+};
+
+Blockly.JavaScript['show_results_in_order'] = function(block) {
+    // 로직을 여기에 작성하세요.
+    var code = 'show_results_in_order();\n';
+    return code;
+};
+
