33e6ba870f773d5a6132833f02773e4734ff859a
eunalove <88129325+eunalove@users.noreply.github.com>
2023-05-29 06:16:17 +0900

    #34 이미지 분류 모델 URL 입력, 학습한 이미지 모델로 분류하기 블록 정상 작동 확인

--- src/main/resources/static/blockEvent.js
+++ src/main/resources/static/blockEvent.js
@@ -1,38 +1,84 @@
+let model, webcam, labelContainer, maxPredictions, url;
+let isIos = false;
+
+
+labelContainer = document.getElementById('label-container');
 
 Blockly.JavaScript['start'] = function(block) {
     console.log("start");
+    if (window.navigator.userAgent.indexOf('iPhone') > -1 || window.navigator.userAgent.indexOf('iPad') > -1) {
+        isIos = true;
+    }
     return '\n';
 };
 
-Blockly.JavaScript['learning_image_ai'] = function(block) {
-    // 로직을 여기에 작성하세요.
-    var code = 'learningImageAI();\n';
-    return code;
-};
 
 Blockly.JavaScript['turn_on_the_video'] = function(block) {
     console.log("turnOnTheVideo");
+    var code = '\n';
+    return code;
 
-    return 'turnOnTheVideo();\n';
 };
 
 Blockly.JavaScript['url_image_receive'] = function(block) {
     console.log("url_image_receive");
-    return 'urlImageReceive();\n';
+
+    url = "https://teachablemachine.withgoogle.com/models/okWkNoAb-k/";
+   // url = Blockly.JavaScript.valueToCode(block, 'url_image', Blockly.JavaScript.ORDER_NONE) || '""';
+
+    console.log(url);
+    return '\n';
+    //return 'urlImageReceive();\n';
 };
 
 
-Blockly.JavaScript['classify_by_trained_model_image'] = function(block) {
+Blockly.JavaScript['classify_by_trained_model_image'] = async function(block) {
     console.log("classifyByTrainedModelImage");
+    console.log("classify_by_trained_model_image 들어옴0");
+    const modelURL = url + 'model.json';
+    const metadataURL = url + 'metadata.json';
+    console.log("classify_by_trained_model_image 들어옴");
+    model = await tmImage.load(modelURL, metadataURL);
+    console.log("classify_by_trained_model_image 끝남");
+    maxPredictions = model.getTotalClasses();
+    console.log(maxPredictions);
+    console.log(model.getClassLabels());
+   // var code = 'classify_by_trained_model_image();\n';
+    return '\n';
 
-    return 'classifyByTrainedModelImage();\n';
+
+    //return 'classifyByTrainedModelImage();\n';
 
 };
 
-Blockly.JavaScript['when_detecting_image'] = function(block) {
-    // 로직을 여기에 작성하세요.
-    var code = 'when_detecting_image();\n';
-    return code;
+// Function to start the video
+async function startVideo() {
+    const flip = true;
+    const width = 500;
+    const height = 500;
+    webcam = new tmImage.Webcam(width, height, flip);
+    await webcam.setup();
+
+    if (isIos) {
+        document.getElementById('webcam-container').appendChild(webcam.webcam);
+        const webCamVideo = document.getElementsByTagName('video')[0];
+        webCamVideo.setAttribute("playsinline", true);
+        webCamVideo.muted = "true";
+        webCamVideo.style.width = width + 'px';
+        webCamVideo.style.height = height + 'px';
+    } else {
+        document.getElementById("webcam-container").appendChild(webcam.canvas);
+    }
+    webcam.play();
+}
+
+Blockly.JavaScript['when_detecting_image'] = async function(block) {
+    await startVideo();
+    for (let i = 0; i < maxPredictions; i++) {
+        labelContainer.appendChild(document.createElement('div'));
+    }
+    window.requestAnimationFrame(startLoop);
+    return '\n';
 };
 
 Blockly.JavaScript['of_confidence_image'] = function(block) {
